31bb561ac690088d6c42aa04376a4b3b06a9eabb
Halamahajne2910 <hala.mahajna@campus.technion.ac.il>
2023-05-17 17:26:25 +0300

    added AVLTree Hala

--- AVLTree.h
+++ AVLTree.h
@@ -7,7 +7,7 @@
 #include <iostream>
 #include "AVLNode.h"
 #include "wet1util.h"
-//
+
 template <class Key, class Data>
 class AVLtree
 {
@@ -26,25 +26,27 @@ private:
     AVLNode<Key,Data> *DeleteNode(const Key &key, AVLNode<Key,Data> *node);
     int BF(AVLNode<Key,Data>* node);
     void UpdateHeight(AVLNode<Key,Data>* node);
-    AVLNode<Key,Data>* FindNode(AVLNode<Key,Data>* currentNode, const Key& key);
+    AVLNode<Key,Data>* FindNode(const Key& key,AVLNode<Key,Data>* node);
     void BuildInOrderArray(AVLNode<Key,Data>* node, Data* InOrderArray);
     void DeleteTree(AVLNode<Key,Data>* node);
 
 
 public:
 
-            AVLtree() : m_size(0), m_root(nullptr){}
-            AVLtree(const AVLtree &other) = delete;
-            AVLtree &operator=(const AVLtree &other) = delete;
-            virtual ~AVLtree();
-            int getSize() const;
-            StatusType Insert(const Key &key, const Data &data);
-            StatusType Remove(const Key &key);
-            Data Find(const Key &key);
-            bool ElementInTree(const Key &key);
+    AVLtree() : m_size(0), m_root(nullptr){}
+    AVLtree(const AVLtree &other) = delete;
+    AVLtree &operator=(const AVLtree &other) = delete;
+    virtual ~AVLtree(){
+        DeleteTree(m_root);
+    }
+    int getSize() const;
+    StatusType Insert(const Key &key, const Data &data);
+    StatusType Remove(const Key &key);
+    Data Find(const Key &key);
+    bool ElementInTree(const Key &key);
 
 };
-            ////////////////////// Implementations for private//////////////
+////////////////////// Implementations for private//////////////
 
 template <class Key ,class Data>
 int AVLtree<Key,Data>::BF(AVLNode<Key,Data> *node)
@@ -55,15 +57,14 @@ template <class Key ,class Data>
 AVLNode<Key,Data> *AVLtree<Key,Data>::RotateRight(AVLNode<Key,Data> *node)
 {
     AVLNode<Key,Data>* newnode = node->m_leftChild;
-    AVLNode<Key,Data>* tempnode = node;
-    node->m_leftChild = newnode->m_rightChild;
-    if (tempnode->m_leftChild != nullptr){
-        tempnode->m_leftChild->m_parent = tempnode;
+    if (newnode->m_rightChild != nullptr){
+        newnode->m_rightChild->m_parent = node;
     }
-    newnode->m_rightChild = tempnode;
-    newnode->m_parent = tempnode->m_parent;
-    tempnode->m_parent = newnode;
-    tempnode->updateParameters();
+    node->m_leftChild = newnode->m_rightChild;
+    newnode->m_rightChild = node;
+    newnode->m_parent = node->m_parent;
+    node->m_parent = newnode;
+    node->updateParameters();
     newnode->updateParameters();
     return newnode;
 
@@ -72,15 +73,14 @@ template <class Key ,class Data>
 AVLNode<Key,Data> *AVLtree<Key,Data>::RotateLeft(AVLNode<Key,Data> *node)
 {
     AVLNode<Key,Data>* newnode = node->m_rightChild;
-    AVLNode<Key,Data>* tempnode = node;
-    node->m_rightChild = newnode->m_leftChild;
-    if (tempnode->m_rightChild != nullptr){
-        tempnode->m_rightChild->m_parent = tempnode;
+    if (newnode->m_leftChild != nullptr){
+        newnode->m_leftChild->m_parent = node;
     }
-    newnode->m_leftChild = tempnode;
-    newnode->m_parent = tempnode->m_parent;
-    tempnode->m_parent = newnode;
-    tempnode->updateParameters();
+    node->m_rightChild = newnode->m_leftChild;
+    newnode->m_leftChild = node;
+    newnode->m_parent = node->m_parent;
+    node->m_parent = newnode;
+    node->updateParameters();
     newnode->updateParameters();
     return newnode;
 
@@ -111,7 +111,7 @@ AVLNode<Key,Data>  *AVLtree<Key,Data>::RollingLR(AVLNode<Key,Data>* node)
 template <class Key ,class Data>
 AVLNode<Key,Data> *AVLtree<Key,Data>:: MakeBalance(AVLNode<Key,Data>* node)
 {
-    int BF = BF(node);
+    int BF = AVLtree<Key,Data>::BF(node);
     if (BF == 2)
     {
         if(BF(node->m_leftChild) >= 0)
@@ -134,6 +134,7 @@ AVLNode<Key,Data> *AVLtree<Key,Data>:: MakeBalance(AVLNode<Key,Data>* node)
             return RollingRR(node);
         }
     }
+    m_root-> m_parent == nullptr;
     return node;
 }
 template <class Key ,class Data>
@@ -151,7 +152,6 @@ AVLNode<Key,Data> *AVLtree<Key,Data>::InsertNode(const Key &key, const Data &dat
         if (m_root == nullptr){
             m_root = newElement;
         }
-        m_size++;
         return newElement;
     }
     else //tree not empty
@@ -159,16 +159,16 @@ AVLNode<Key,Data> *AVLtree<Key,Data>::InsertNode(const Key &key, const Data &dat
         if(key > node->m_key)
         {
             AVLNode<Key,Data>* newNode = InsertNode(key, data,node->m_rightChild);
-            newNode->m_parent_node = node;
+            newNode->m_parent = node;
             node->m_rightChild = newNode;
-            node->m_rightChild->m_parent_node = node;
+            node->m_rightChild->m_parent= node;
         }
         else if(key < node->m_key)
         {
             AVLNode<Key,Data>* newNode = InsertNode(node->m_leftChild, key, data);
-            newNode->m_parent_node = node;
+            newNode->m_parent = node;
             node->m_leftChild = newNode;
-            node->m_leftCHild->m_parent_node = node;
+            node->m_leftCHild->m_parent= node;
         }
     }
     node->updateHeight();
@@ -178,12 +178,145 @@ AVLNode<Key,Data> *AVLtree<Key,Data>::InsertNode(const Key &key, const Data &dat
 template <class Key ,class Data>
 AVLNode<Key,Data> *AVLtree<Key,Data>::DeleteNode(const Key &key, AVLNode<Key, Data> *node)
 {
-    
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    if(node->m_key == key)
+
+    {
+        AVLNode<Key,Data>* tempNode;
+        if ((node->m_leftChild ) && (node->m_rightChild )) // two SONS
+        {
+            tempNode = node->m_rightChild;
+            while (tempNode->m_leftChild) {
+                tempNode = tempNode->m_leftChild;
+            }
+            tempNode->swap(node);
+            node->m_rightChild = DeleteNode(key,node->m_rightChild);
+        }
+
+        else if ((node->m_leftChild == nullptr) || (node->m_rightChild == nullptr)) //ONE CHILD ONLY
+        {
+            if (node->m_leftChild)
+            {
+                tempNode = node->m_leftChild;
+            }
+            else
+            {
+                tempNode = node->m_rightChild;
+            }
+            tempNode->m_parent = node->m_parent;
+            delete node;
+            node = tempNode;
+        }
+        else //no sons
+        {
+            delete node;
+            node = nullptr;
+            return node;
+        }
+    }
+    else if (node->m_key < key)
+    {
+        node->m_rightChild = DeleteNode(key,node->m_rightChild);
+    }
+    else if (node->m_key > key)
+    {
+        node->m_leftChild = DeleteNode(key,node->m_leftChild);
+    }
+    node->updateParameters();
+    return MakeBalance(node);
 }
+template <class Key ,class Data>
+AVLNode<Key,Data> *AVLtree<Key,Data>:: FindNode(const Key& key,AVLNode<Key,Data>* node){
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    if(key == node->m_key)
+    {
+        return node;
+    }
+    else if(key > node->m_key)
+    {
+        return FindNode(key,node->m_rightChild);
+    }
+    else if(key < node-> m_key)
+    {
+        return FindNode(key,node->m_leftChild);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+template <class Key ,class Data>
+void AVLtree<Key,Data>::DeleteTree(AVLNode<Key,Data> *node)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    DeleteTree(node->m_rightChild);
+    DeleteTree(node->m_leftChild);
+    delete node;
+}
+
 ////////////////////// Implementations for public//////////////
 
+template <class Key ,class Data>
+StatusType AVLtree<Key,Data>::Insert(const Key &key, const Data &data)
+{
 
+    if (this->FindNode(key))
+    {
+        return StatusType::FAILURE;
+    }
+    m_root = InsertNode(key, data,m_root);
+    if(m_root == nullptr)
+    {
+        return StatusType::ALLOCATION_ERROR;
+    }
+    m_size++;
+    return StatusType::SUCCESS;
+}
+template <class Key ,class Data>
+StatusType AVLtree<Key,Data>::Remove(const Key &key)
+{
 
+    if (FindNode(key,m_root) == nullptr){
+        return StatusType::FAILURE;
+    }
+    m_root = DeleteNode(key,m_root);
+    m_size--;
+    return StatusType::SUCCESS;
+
+}
+template <class Key ,class Data>
+Data AVLtree<Key,Data>::Find(const Key& key)
+{
+    AVLNode<Key,Data>* tmp = FindNode(key,m_root);
+    if (tmp == nullptr){
+        return nullptr;
+    }
+    return tmp->m_data;
+}
+template <class Key ,class Data>
+bool AVLtree<Key,Data>:: ElementInTree(const Key& key){
+    if (m_root == nullptr){//tree empty
+        return false;
+    }
+    if (this->FindNode(key,m_root) == nullptr){
+        return false;
+    }else{
+        return true;
+    }
+}
+template <class Key ,class Data>
+int AVLtree<Key,Data>::getSize() const {
+    return m_size;
+}
 
 
 
