1d88e81ae6127d6703ec1fcd5ceeb113422c0b07
sabry assaf <91211033+sabryassaf@users.noreply.github.com>
2023-05-18 13:01:58 +0300

    fixed the key class as we discussed and fixed git ignore abit

--- Key.h
+++ Key.h
@@ -9,11 +9,6 @@
 #include <exception>
 #include <string>
 
-enum struct OrderRule
-{
-    ID, RATING
-};
-
 class Key_Type_Exceptions : public std::exception
 {
     std::string m_message;
@@ -27,112 +22,61 @@ public:
     }
 };
 
-class Key
+class MovieOrderById
 {
-protected:
     int m_id;
-public:
-    Key(int id) : m_id(id)
-    {}
-
-    int getId() const
-    {
-        return m_id;
-    }
-
-    virtual bool operator<(const Key &other) const = 0;
-
-    virtual bool operator==(const Key &other) const = 0;
-
-    virtual bool operator>(const Key &other) const = 0;
-
-};
-
-
-class UserId : public Key
-{
-public:
-    UserId(int id) : Key(id)
-    {}
-
-    bool operator<(const Key &other) const override
-    {
-        const UserId *otherUser = dynamic_cast<const UserId *>(&other);
-        if (otherUser != nullptr)
-        {
-            return this->m_id < otherUser->m_id;
-        } else
-        {
-            throw (Key_Type_Exceptions("The Data Types Aren't comparable "));
-        }
-
-    }
-
-    bool operator==(const Key &other) const override
-    {
-        const UserId *otherUser = dynamic_cast<const UserId *>(&other);
-        if (otherUser != nullptr)
-        {
-            return this->m_id == otherUser->m_id;
-        } else
-        {
-            throw (Key_Type_Exceptions("The Data Types Aren't comparable "));
-        }
-
-    }
-
-    bool operator>(const Key &other) const override
-    {
-        const UserId *otherUser = dynamic_cast<const UserId *>(&other);
-        if (otherUser != nullptr)
-        {
-            return this->m_id > otherUser->m_id;
-        } else
-        {
-            throw (Key_Type_Exceptions("The Data Types Aren't comparable "));
-        }
-
-    }
-};
-
-class MovieId : public Key
-{
     int m_rating;
     int m_viewers;
-    OrderRule m_rule;
 public:
-    MovieId(int id, OrderRule &rule) : Key(id), m_rating(0), m_viewers(0), m_rule(rule)
+    MovieOrderById(int id, int views) : m_id(id), m_rating(0), m_viewers(views)
     {}
 
-    bool operator<(const Key &other) const override
+    bool operator<(const MovieOrderById &other) const
     {
-        const MovieId *otherMovie = dynamic_cast<const MovieId *>(&other);
+        const MovieOrderById *otherMovie = dynamic_cast<const MovieOrderById *>(&other);
         if (otherMovie != nullptr)
         {
-            switch (this->m_rule)
+            return this->m_id < otherMovie->m_id;
+        } else
+        {
+            throw (Key_Type_Exceptions("The Data Types Aren't comparable "));
+        }
+
+    }
+};
+
+class MovieOrderByRating
+{
+    int m_id;
+    int m_rating;
+    int m_viewers;
+public:
+    MovieOrderByRating(int id, int views) : m_id(id), m_rating(0), m_viewers(views)
+    {}
+
+    bool operator<(const MovieOrderByRating &other) const
+    {
+        const MovieOrderByRating *otherMovie = dynamic_cast<const MovieOrderByRating *>(&other);
+        if (otherMovie != nullptr)
+        {
+            if (this->m_rating > otherMovie->m_rating)
             {
-                case OrderRule::ID:
+                return true;
+            } else if (this->m_rating < otherMovie->m_rating)
+            {
+                return false;
+            } else
+            {
+                if (this->m_viewers > otherMovie->m_viewers)
+                {
+                    return true;
+                } else if (this->m_viewers < otherMovie->m_viewers)
+                {
+                    return false;
+                } else
+                {
                     return this->m_id < otherMovie->m_id;
-                case OrderRule::RATING:
-                    if (this->m_rating > otherMovie->m_rating)
-                    {
-                        return true;
-                    } else if (this->m_rating < otherMovie->m_rating)
-                    {
-                        return false;
-                    } else
-                    {
-                        if (this->m_viewers > otherMovie->m_viewers)
-                        {
-                            return true;
-                        } else if (this->m_viewers < otherMovie->m_viewers)
-                        {
-                            return false;
-                        } else
-                        {
-                            return this->m_id < otherMovie->m_id;
-                        }
-                    }
+                }
             }
 
         } else
@@ -142,5 +86,52 @@ public:
 
     }
 };
+
+//
+//class UserId : public Key
+//{
+//public:
+//    UserId(int id) : Key(id)
+//    {}
+//
+//    bool operator<(const Key &other) const override
+//    {
+//        const UserId *otherUser = dynamic_cast<const UserId *>(&other);
+//        if (otherUser != nullptr)
+//        {
+//            return this->m_id < otherUser->m_id;
+//        } else
+//        {
+//            throw (Key_Type_Exceptions("The Data Types Aren't comparable "));
+//        }
+//
+//    }
+//
+//    bool operator==(const Key &other) const override
+//    {
+//        const UserId *otherUser = dynamic_cast<const UserId *>(&other);
+//        if (otherUser != nullptr)
+//        {
+//            return this->m_id == otherUser->m_id;
+//        } else
+//        {
+//            throw (Key_Type_Exceptions("The Data Types Aren't comparable "));
+//        }
+//
+//    }
+//
+//    bool operator>(const Key &other) const override
+//    {
+//        const UserId *otherUser = dynamic_cast<const UserId *>(&other);
+//        if (otherUser != nullptr)
+//        {
+//            return this->m_id > otherUser->m_id;
+//        } else
+//        {
+//            throw (Key_Type_Exceptions("The Data Types Aren't comparable "));
+//        }
+//
+//    }
+//};
 
 #endif //WET1_KEY_H
